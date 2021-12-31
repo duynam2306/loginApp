4f661c15c21bde81a9418be9f6e3b8eb1eb2a0e3
duynam2306 <96418879+duynam2306@users.noreply.github.com>
2021-12-31 16:10:51 +0700

    Update list-user.component.ts

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -26,10 +26,7 @@ export class ListUserComponent implements OnInit {
     this.userService.urlCurrent.subscribe(url => this.statusUrl = url);
     console.log(this.users);
   }
-  
-  // 
-  checkUrl!: string;
-
+ 
   // routeLink to loginScreen
   checkUrl!: string;
 
@@ -42,11 +39,7 @@ export class ListUserComponent implements OnInit {
     // Update listUser
     this.userService.changeListUser(this.users);
     console.log(this.users);
-<<<<<<< HEAD
-    // When listUser has 1 user, delete user = log out
-=======
     // When user list has 1 user, delete user = log out
->>>>>>> upstream/main
     if (this.users.length == 1) {
       this.checkUrl = "/loginScreen";
     }
