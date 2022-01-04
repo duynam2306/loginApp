4e13ed105be2ffaf8c1a2cb7c9efed51edad6b14
duynam2306 <96418879+duynam2306@users.noreply.github.com>
2022-01-04 10:45:52 +0700

    Update add-update-user.component.ts

--- src/app/add-update-user/add-update-user.component.ts
+++ src/app/add-update-user/add-update-user.component.ts
@@ -16,17 +16,15 @@ export class AddUpdateUserComponent implements OnInit {
   @ViewChild('checkButton')
   checkButton!: ElementRef;
 
-  // routeLink to listUserComponent
-  userUrl!: string;
   // message of create box
-  messageCreate!: string;
-  messageCreateError!: string;
+  public messageCreate!: string;
+  public messageCreateError!: string;
   // listUser
-  users!: User[];
+  private users!: User[];
   // id of user was click
-  id!: string;
+  private id!: string;
   // status = "add"
-  statusUrl!: string;
+  public statusUrl!: string;
 
   constructor(private userService: UserService) {
 
@@ -36,11 +34,10 @@ export class AddUpdateUserComponent implements OnInit {
     this.userService.usersCurrent.subscribe(users => this.users = users);
     this.userService.idCurrent.subscribe(id => this.id = id);
     this.userService.urlCurrent.subscribe(url => this.statusUrl = url);
-    this.getIndex();
     this.getUserUpdate();
   }
 
-  showPassword(e: any) {
+  public showPassword(e: any) {
     //Delay
     setTimeout(() => {
       // If type of password is password (checkbox isn't checked)
@@ -54,17 +51,56 @@ export class AddUpdateUserComponent implements OnInit {
     }, 100);
   }
 
+  // New user = user at index
+  public userUpdate: User = {
+    id: '',
+    fullname: '',
+    username: '',
+    password: '',
+    checkDelete: false,
+  }
+
+  // Get user need to be updated
+  private getUserUpdate(): void {
+    this.userUpdate = {
+      id: this.users[Number(this.id)].id,
+      fullname: this.users[Number(this.id)].fullname,
+      username: this.users[Number(this.id)].username,
+      password: this.users[Number(this.id)].password,
+      checkDelete: false,
+    }
+  }
+
+  // Message of update box
+  public messageUpdate!: string;
+  public messageUpdateError!: string;
+
+  // Function update user when Update is clicked in Update box
+  // statusUrl = "update"
+  public updateUser(user: User) {
+    this.messageUpdate = '';
+    // If form input is valid
+    if (this.userUpdate.fullname !== '' && this.userUpdate.username !== '' && this.userUpdate.password !== '') {
+      // Set user at index = user at update input form
+      this.users[Number(this.id)] = this.userUpdate;
+      // Update listUser
+      this.userService.changeListUser(this.users);
+      // Message
+      this.messageUpdate = "Updated";
+    } 
+  }
+
   // New user = user at signup input form
-  userSignUp: User = {
+  public userSignUp: User = {
     id: '',
     fullname: '',
     username: '',
     password: '',
     checkDelete: false
   }
-  
+
   // statusUrl = "add"
-  createUser(user: User): void {
+  public createUser(user: User): void {
     this.messageCreate = '';
     this.messageCreateError = '';
     // Create unique id
@@ -79,7 +115,7 @@ export class AddUpdateUserComponent implements OnInit {
     if (this.userSignUp.fullname !== '' && this.userSignUp.username !== '' && this.userSignUp.password !== '') {
       // Check username is exist
       var tempIndex = this.users.findIndex(user => user.username === this.userSignUp.username);
-      // If username is exist in listUser
+      console.log(tempIndex);
       if (tempIndex >= 0) {
         this.messageCreateError = 'Username is exist';
       } else {
@@ -89,11 +125,11 @@ export class AddUpdateUserComponent implements OnInit {
         this.userService.changeListUser(this.users);
         this.messageCreate = "Sign Up Success";
       }
-    } else 
-    // Form input is invalid
-    if (this.userSignUp.fullname === '' && this.userSignUp.username === '' && this.userSignUp.password === '') {
-      this.messageCreateError = 'Fullname, Username and Password are invalid';
-    };
+    } else
+      // Form input is invalid
+      if (this.userSignUp.fullname === '' && this.userSignUp.username === '' && this.userSignUp.password === '') {
+        this.messageCreateError = 'Fullname, Username and Password are invalid';
+      };
 
     this.userSignUp = {
       id: '',
@@ -102,60 +138,12 @@ export class AddUpdateUserComponent implements OnInit {
       password: '',
       checkDelete: false
     };
-    // routing to listUserComponent
-    this.userUrl = "/listUser";
   }
 
-  // Index of user is clicked
-  index!: number;
-
-  // Get index of user updated 
-  getIndex(): number {
-    this.index = Number(this.id);
-    return this.index;
-  }
-
-  // New user = user at index
-  userUpdate: User = {
-    id: '',
-    fullname: '',
-    username: '',
-    password: '',
-    checkDelete: false,
-  }
-
-  // Get user need to be updated
-  getUserUpdate(): void {
-    this.userUpdate = {
-      id: this.users[this.getIndex()].id,
-      fullname: this.users[this.getIndex()].fullname,
-      username: this.users[this.getIndex()].username,
-      password: this.users[this.getIndex()].password,
-      checkDelete: false,
-    }
-  }
-
-  // Message of update box
-  messageUpdate!: string;
-  messageUpdateError!: string;
-
-  // Function update user when Update is clicked in Update box
-  // statusUrl = "update"
-  updateUser(user: User) {
-    this.messageUpdate = '';
-    // If form input is valid
-    if (this.userUpdate.fullname !== '' && this.userUpdate.username !== '' && this.userUpdate.password !== '') {
-      // Set user at index = user at update input form
-      this.users[this.getIndex()] = this.userUpdate;
-      // Update listUser
-      this.userService.changeListUser(this.users);
-      // Message
-      this.messageUpdate = "Updated";
-    } 
-  }
-
+  
   // Function set statusUrl = "add" when click Cancel
-  changeStatus() {
+  public changeStatus() {
     this.userService.changeUrl("add");
   }
+
 }
